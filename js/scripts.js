09a08a9359e80763c5f875bd458009a901b8e83e
Student <example@example.com>
2019-12-16 19:03:51 -0800

    add html, css and scripts that shows more functionality to project. Code is working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
-
-    var leapYear = function(year) {
+// business logic
+var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
@@ -10,10 +7,21 @@ $(document).ready(function() {
   }
 };
 
-// jQuery user interface logic here.
-
+// user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
